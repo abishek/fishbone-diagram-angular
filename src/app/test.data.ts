f4abbb6b36459effbbf69704f20705ad6feb52ed
Abishek Goda <abishek.goda@hey.com>
2021-09-22 07:41:43 +0800

    More sample test data.

--- src/app/test.data.ts
+++ src/app/test.data.ts
@@ -1,42 +1,54 @@
-export const testData = {
+export const testData2 = {
     "name": "Quality",
     "children": [
         {
+            "id": "1",
             "name": "Machine",
             "children": [
                 {
+                    "id": "2",
                     "name": "Mill"
                 },
                 {
+                    "id": "3",
                     "name": "Mixer"
                 },
                 {
+                    "id": "4",
                     "name": "Metal Lathe"
                 }
             ]
         },
         {
+            "id": "5",
             "name": "Method"
         },
         {
+            "id": "6",
             "name": "Material",
             "children": [
                 {
+                    "id": "7",
                     "name": "Masonite"
                 },
                 {
+                    "id": "8",
                     "name": "Marscapone",
                     "children": [
                         {
+                            "id": "9",
                             "name": "Malty"
                         },
                         {
+                            "id": "10",
                             "name": "Minty",
                             "children": [
                                 {
+                                    "id": "11",
                                     "name": "spearMint"
                                 },
                                 {
+                                    "id": "12",
                                     "name": "pepperMint"
                                 }
                             ]
@@ -44,9 +56,11 @@ export const testData = {
                     ]
                 },
                 {
+                    "id": "13",
                     "name": "Meat",
                     "children": [
                         {
+                            "id": "14",
                             "name": "Mutton"
                         }
                     ]
@@ -54,35 +68,45 @@ export const testData = {
             ]
         },
         {
+            "id": "15",
             "name": "Man Power",
             "children": [
                 {
+                    "id": "16",
                     "name": "Manager"
                 },
                 {
+                    "id": "17",
                     "name": "Master's Student"
                 },
                 {
+                    "id": "18",
                     "name": "Magician"
                 },
                 {
+                    "id": "19",
                     "name": "Miner"
                 },
                 {
+                    "id": "20",
                     "name": "Magister",
                     "children": [
                         {
+                            "id": "21",
                             "name": "Malpractice"
                         }
                     ]
                 },
                 {
+                    "id": "22",
                     "name": "Massage Artist",
                     "children": [
                         {
+                            "id": "23",
                             "name": "Masseur"
                         },
                         {
+                            "id": "24",
                             "name": "Masseuse"
                         }
                     ]
@@ -90,20 +114,31 @@ export const testData = {
             ]
         },
         {
+            "id": "25",
             "name": "Measurement",
             "children": [
                 {
+                    "id": "26", 
                     "name": "Malleability"
                 }
             ]
         },
         {
+            "id": "27",
             "name": "Milieu",
             "children": [
                 {
+                    "id": "28",
                     "name": "Marine"
                 }
             ]
         }
+    ]
+}
+
+export const testData = {
+    "name": "Quality",
+    "uuid": "1",
+    "children": [ 
     ]
 }
